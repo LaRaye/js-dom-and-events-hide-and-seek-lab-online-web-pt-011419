ce418ab32ed21f3c2a13d8c4b8a2233b22ae6cfa
LaRaye Johnson <lnj2105@gmail.com>
2019-09-06 20:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   const ranks = document.getElementById('ul.ranked-list li');
   
   for (let i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML + parseInt(n));
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + parseInt(n);
   }
 }
